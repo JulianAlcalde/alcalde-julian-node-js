116ea8102dc024a79955654b7bf7e9892ad50eca
Julián <127742213+JulianAlcalde@users.noreply.github.com>
2023-06-05 08:15:07 -0300

    ejercicio-2-node-js
    ejercicio-2-node-js

---
+++ ej2.js
@@ -0,0 +1,8 @@
+const prompt=require("prompt-sync")();
+
+let num1=parseInt(prompt("Ingrese un número: "));
+let num2=parseInt(prompt("Ingrese un número: "));
+
+let suma=num1+num2
+
+console.log(suma)
