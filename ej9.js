a00d2515fcd9106615b9b2fa93a85b5138c2a334
Julián <127742213+JulianAlcalde@users.noreply.github.com>
2023-06-05 08:17:35 -0300

    ejercicio-9-node-js
    ejercicio-9-node-js

---
+++ ej9.js
@@ -0,0 +1,13 @@
+const prompt=require("prompt-sync")();
+
+let edad = parseInt(prompt("Introduce una edad: "));
+
+if (edad >= 0 && edad < 13) {
+  console.log("Es un niño");
+} else if (edad >= 13 && edad <= 17) {
+  console.log("Es un adolescente");
+} else if (edad >= 18 && edad <= 110) {
+  console.log("Es un adulto");
+} else {
+  console.log("No es una edad válida");
+}
